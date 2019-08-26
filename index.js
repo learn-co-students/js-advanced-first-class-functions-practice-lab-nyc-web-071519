a235741d3f1f76ec73bf923e351da84df21e0c48
Andrew Santos <asantos@alumni.nd.edu>
2019-08-26 13:49:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+    for (const driver of drivers){
+        console.log(driver.name);
+    }
+}
+
+function logDriversByHometown(drivers, loc){
+    for (const driver of drivers){
+        if (driver.hometown === loc){
+            console.log(driver.name);
+        }
+    }
+}
+
+function driversByRevenue(drivers){
+    const temp = drivers.slice()
+    temp.sort(function (a,b){
+        return a.revenue - b.revenue;
+    });
+    return temp;
+}
+
+function driversByName(drivers){
+    const temp = drivers.slice()
+    temp.sort(function (a,b){
+        return a.name.localeCompare(b.name);
+    });
+    return temp;
+}
+
+function totalRevenue(drivers){
+    const reduceFunc = function (agg, el, i, arr) {
+        return agg += el.revenue;
+    }
+    return drivers.reduce (reduceFunc, 0);
+}
+
+function averageRevenue(drivers){
+    return totalRevenue(drivers) / drivers.length
+}
